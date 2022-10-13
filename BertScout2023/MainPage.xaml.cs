9caa00489554fc7ba8216c489d2d2852a6c7e203
Scott Bakker <sbakker343@gmail.com>
2022-10-12 22:01:19 -0400

    001

--- BertScout2023/MainPage.xaml.cs
+++ BertScout2023/MainPage.xaml.cs
@@ -1,14 +1,36 @@
-﻿namespace BertScout2023;
+﻿using System.Text;
+
+namespace BertScout2023;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void Start_Clicked(object sender, EventArgs e)
-	{
+    private void Start_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(TeamNumber.Text))
+        {
+            StartLabel.Text = "Please enter a team number";
+        }
+        else
+        {
+            StartLabel.Text = "Hello Team " + TeamNumber.Text;
+        }
+    }
 
-	}
+    private void TeamNumber_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        StringBuilder s = new();
+        foreach (char c in TeamNumber.Text)
+        {
+            if (char.IsNumber(c)) s.Append(c);
+        }
+        if (s.Length != TeamNumber.Text.Length)
+        {
+            TeamNumber.Text = s.ToString();
+        }
+    }
 }

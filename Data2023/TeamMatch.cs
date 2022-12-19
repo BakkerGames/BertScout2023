ab7c4a6a0da8dbf8be7ef15dda8529251b93dd9e
Scott Bakker <sbakker343@gmail.com>
2022-12-18 19:59:14 -0500

    Added data proj

---
+++ Data2023/TeamMatch.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+
+namespace Data2023;
+
+// IMPORTANT! Increment DatabaseServices.dbVersion whenever this changes!
+
+public class TeamMatch
+{
+    // metadata
+    [PrimaryKey, AutoIncrement]
+    public int? Id { get; set; }
+    public string Uuid { get; set; } = "";
+    public string AirtableId { get; set; } = "";
+    public bool Changed { get; set; }
+    public bool Deleted { get; set; }
+
+    [Indexed]
+    public int TeamNumber { get; set; }
+    [Indexed]
+    public int MatchNumber { get; set; }
+    public string ScouterName { get; set; } = "";
+
+    // autonomous
+
+    // teleop
+
+    // end game
+
+    // overall
+    public string Comments { get; set; } = "";
+}

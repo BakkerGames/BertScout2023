9c63d8c6eb02d6577ff6ba1eb2ef509ba5317a8e
Scott Bakker <sbakker343@gmail.com>
2023-01-22 12:19:43 -0500

    Airtable results

--- BertScout2023/AirtablePage.xaml.cs
+++ BertScout2023/AirtablePage.xaml.cs
@@ -2,8 +2,24 @@ namespace BertScout2023;
 
 public partial class AirtablePage : ContentPage
 {
-	public AirtablePage()
-	{
-		InitializeComponent();
-	}
+    public AirtablePage()
+    {
+        InitializeComponent();
+    }
+
+    private void AirtableSend_Clicked(object sender, EventArgs e)
+    {
+        AirtableResults.Text = "";
+        for (int i = 0; i < 100; i++)
+        {
+            if (AirtableResults.Text.Length > 0)
+                AirtableResults.Text += "\r\n";
+            AirtableResults.Text += $"Line {i} - Message";
+        }
+    }
+
+    private void VerticalStackLayout_SizeChanged(object sender, EventArgs e)
+    {
+        ScrollResults.HeightRequest = cp.Height - ScrollResults.Y;
+    }
 }

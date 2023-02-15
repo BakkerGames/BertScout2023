a913a3b0bcb4cb292c21d1636a4019e9010a5c21
Scott <Scott@MEOWPC>
2023-02-15 17:41:25 -0500

    start of airtable

--- BertScout2023/AirtablePage.xaml.cs
+++ BertScout2023/AirtablePage.xaml.cs
@@ -1,20 +1,27 @@
+using BertScout2023.Databases;
+using BertScout2023.Models;
+
 namespace BertScout2023;
 
 public partial class AirtablePage : ContentPage
 {
+    private readonly LocalDatabase db = new();
+
     public AirtablePage()
     {
         InitializeComponent();
     }
 
-    private void AirtableSend_Clicked(object sender, EventArgs e)
+    private async void AirtableSend_Clicked(object sender, EventArgs e)
     {
         AirtableResults.Text = "";
-        for (int i = 0; i < 100; i++)
+        List<TeamMatch> matches = await db.GetItemsAsync();
+        foreach (TeamMatch item in matches
+            .OrderBy(x => $"{x.MatchNumber,3}{x.MatchNumber,4}"))
         {
             if (AirtableResults.Text.Length > 0)
                 AirtableResults.Text += "\r\n";
-            AirtableResults.Text += $"Line {i} - Message";
+            AirtableResults.Text += $"{item.MatchNumber,4} - {item.TeamNumber,4}";
         }
     }
 
